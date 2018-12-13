351491164f2042e0f74c76383b1656adc15e9fc0
Artem Bolgar <Artem.Bolgar@oculus.com>
2018-12-12 23:22:17 -0800

    Workaround for #15422

--- src/audio/PositionalAudio.js
+++ src/audio/PositionalAudio.js
@@ -104,6 +104,10 @@ PositionalAudio.prototype = Object.assign( Object.create( Audio.prototype ), {
 
 			Object3D.prototype.updateMatrixWorld.call( this, force );
 
+			if ( !this.isPlaying ) {
+				return;
+			}
+
 			var panner = this.panner;
 			this.matrixWorld.decompose( position, quaternion, scale );
 
